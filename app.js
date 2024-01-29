a1660b2291a98b6474663a8085f21f745796c869
chel12 <sema.chel12@gmail.com>
2024-01-29 11:13:45 +0300

    роуты сделаны, экспорт функций идёт из controllers/users.js

--- app.js
+++ app.js
@@ -1,26 +1,27 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+//чтоб экс пресс увидел .env с портом 8000
+require('dotenv').config();
 
-var app = express();
+const usersRouter = require('./routes/users');
+
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-app.use(logger('dev'));
-app.use(express.json());
+app.use(logger('dev')); //подкл логгер
+app.use(express.json());//посылать в запрос тело
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(cookieParser()); //парс куки, не буду использовать, но пусть будет
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+
+app.use('/api/user', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,13 @@
-var express = require('express');
-var router = express.Router();
-
+const express = require('express');
+const router = express.Router();
+const { login, register, current } = require('../controllers/users');
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+//функции вынесены в controllers / users.js и экспортируются оттуда
+/*./api/user/Login */
+router.post('/login', login);
+/*./api/user/register */
+router.post('/register', register);
+/*./api/user/current */
+router.get('/current', current);
 
 module.exports = router;
