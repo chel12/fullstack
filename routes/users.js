98b2cb54edab9d7f14538eebab5437f8a2ef926b
chel12 <sema.chel12@gmail.com>
2024-01-30 14:30:04 +0300

    api: login + registration + hashPassword+token

--- routes/users.js
+++ routes/users.js
@@ -3,11 +3,11 @@ const router = express.Router();
 const { login, register, current } = require('../controllers/users');
 /* GET users listing. */
 //функции вынесены в controllers / users.js и экспортируются оттуда
-/*./api/user/Login */
+// /api/user/Login
 router.post('/login', login);
-/*./api/user/register */
+// /api/user/register
 router.post('/register', register);
-/*./api/user/current */
+// /api/user/current
 router.get('/current', current);
 
 module.exports = router;
