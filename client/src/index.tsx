6e62e77e3ae65e0febf4668fc52c1a212f015cbc
chel12 <sema.chel12@gmail.com>
2024-01-31 15:56:08 +0300

    front:pages:Login+Register+настроен на них router-provider

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,20 +1,40 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { Paths } from './paths';
 import { store } from './app/store';
+import Login from './pages/Login/Login';
+import Register from './pages/Register/Register';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import './index.css';
+
+//роутер массив объектов с путями
+const router = createBrowserRouter([
+	{
+		path: Paths.home,
+		element: <App />,
+	},
+	{
+		path: Paths.login,
+		element: <Login />,
+	},
+	{
+		path: Paths.register,
+		element: <Register />,
+	},
+]);
 
 const container = document.getElementById('root')!;
 const root = createRoot(container);
 
 root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>
+	<React.StrictMode>
+		<Provider store={store}>
+			<RouterProvider router={router} />
+		</Provider>
+	</React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
