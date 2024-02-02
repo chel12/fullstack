0daf9fa49d45d1d0763329a5e4f1cba96c5fc0fc
chel12 <sema.chel12@gmail.com>
2024-02-02 10:35:04 +0300

    pages Register success

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,7 @@
 import React from 'react';
 
-
 function App() {
-  return (
-    <div className="App">
-     <h1>
-		hi
-	  </h1>
-    </div>
-  );
+	return <div className="App"></div>;
 }
 
 export default App;
--- client/src/pages/Register/Register.tsx
+++ client/src/pages/Register/Register.tsx
@@ -1,10 +1,44 @@
+import { Card, Form, Row, Space, Typography } from 'antd';
 import React from 'react';
+import { Link } from 'react-router-dom';
+import CustomButton from '../../components/CustomButton/CustomButton';
+import CustomInput from '../../components/CustomInput/CustomInput';
+import CustomInputPassword from '../../components/CustomInputPassword/CustomInputPassword';
+import { Paths } from '../../paths';
 import Layout from '../../components/Layout/Layout';
 
 const Register = () => {
 	return (
 		<Layout>
-			<div>Register</div>
+			<Row align="middle" justify="center">
+				<Card title="Зарегестрируйтесь" style={{ width: '30rem' }}>
+					<Form onFinish={() => null}>
+						<CustomInput name="name" placeholder="Имя" />
+						<CustomInput
+							type="email"
+							name="email"
+							placeholder="Email"
+						/>
+						<CustomInputPassword
+							name="password"
+							placeholder="Пароль"
+						/>
+						<CustomInputPassword
+							name="confirmPassword"
+							placeholder="Повторите пароль"
+						/>
+						<CustomButton type="primary" htmlType="submit">
+							Зарегестрироваться
+						</CustomButton>
+					</Form>
+					<Space direction="vertical" size="large">
+						<Typography.Text>
+							Уже зарегестрированы?{' '}
+							<Link to={Paths.login}>Войти</Link>
+						</Typography.Text>
+					</Space>
+				</Card>
+			</Row>
 		</Layout>
 	);
 };
