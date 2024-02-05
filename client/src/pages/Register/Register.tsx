7ef2894e10da8d71e6da4dadb754de565755aad8
chel12 <sema.chel12@gmail.com>
2024-02-05 14:46:15 +0300

    App success

--- client/src/pages/Register/Register.tsx
+++ client/src/pages/Register/Register.tsx
@@ -1,18 +1,45 @@
 import { Card, Form, Row, Space, Typography } from 'antd';
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 import CustomButton from '../../components/CustomButton/CustomButton';
 import CustomInput from '../../components/CustomInput/CustomInput';
 import CustomInputPassword from '../../components/CustomInputPassword/CustomInputPassword';
 import { Paths } from '../../paths';
 import Layout from '../../components/Layout/Layout';
+import { useSelector } from 'react-redux';
+import { selectUser } from '../../features/auth/authSlice';
+import { useRegisterMutation } from '../../app/services/auth';
+import { User } from '@prisma/client';
+import { isErrorWithMessage } from '../../utils/isErrorWithMessage';
+import ErrorMessage from '../../components/ErrorMessage/ErrorMessage';
+
+type RegisterData = Omit<User, 'id'> & { confirmPassword: string };
 
 const Register = () => {
+	const navigate = useNavigate();
+	const user = useSelector(selectUser);
+	const [error, setError] = useState('');
+	const [registeruser] = useRegisterMutation();
+
+	const register = async (data: RegisterData) => {
+		try {
+			await registeruser(data).unwrap();
+			navigate('/');
+		} catch (error) {
+			const maybeError = isErrorWithMessage(error);
+			if (maybeError) {
+				setError(error.data.message);
+			} else {
+				setError('Неизвестная ошибка');
+			}
+		}
+	};
+
 	return (
 		<Layout>
 			<Row align="middle" justify="center">
 				<Card title="Зарегестрируйтесь" style={{ width: '30rem' }}>
-					<Form onFinish={() => null}>
+					<Form onFinish={register}>
 						<CustomInput name="name" placeholder="Имя" />
 						<CustomInput
 							type="email"
@@ -36,6 +63,7 @@ const Register = () => {
 							Уже зарегестрированы?{' '}
 							<Link to={Paths.login}>Войти</Link>
 						</Typography.Text>
+						<ErrorMessage message={error}></ErrorMessage>
 					</Space>
 				</Card>
 			</Row>
